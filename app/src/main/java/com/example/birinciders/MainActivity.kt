4b9ce0a5c6d5e8b19d575af746a2cd2f3ad8afd6
SefaTns <77925032+SefaTns@users.noreply.github.com>
2025-04-30 18:08:04 +0300

    Odev3 tamamlandi

--- app/src/main/java/com/example/birinciders/MainActivity.kt
+++ app/src/main/java/com/example/birinciders/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.odev3)
         // R (Kotlin sınıfları içerisinde)= res klasörü, kaynaklar
         // Q (XML dosyaları içerisinde)= res klasörü, kaynaklar
     }
